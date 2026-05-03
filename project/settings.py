93445eecc816173b9ef637a4a81db1f297be0176
Mandip <mandipadhikari.codes@gmail.com>
2026-05-03 15:53:16 +0545

    fix: use STORAGES setting for cloudinary in Django 6.0

--- project/settings.py
+++ project/settings.py
@@ -79,7 +79,14 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 MIDDLEWARE = [
