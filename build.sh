2508223f67063b0f5b5e41dab102ac1556d1ca1f
Mandip <mandipadhikari.codes@gmail.com>
2026-05-12 19:14:09 +0545

    fix: fake broken cart migrations on deploy

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+python manage.py migrate store 0009 --fake || true
+python manage.py migrate store 0010 --fake || true
 python manage.py migrate
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
     python manage.py createsuperuser --noinput || true
 fi
+
