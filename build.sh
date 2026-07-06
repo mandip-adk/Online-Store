e65f03aa4004a1163d38000c2ce9788edcdc003d
Mandip <mandipadhikari.codes@gmail.com>
2026-07-06 13:33:09 +0545

    fix: remove fake migrations from deployment

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate accounts 0005 --fake || true
-python manage.py migrate store 0019 --fake || true
 python manage.py migrate
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
